024873e5e21ca6c9506927a4d76a776fed2b9da4
jeongsangtae <wjdtkdxo55@naver.com>
2022-12-10 22:12:03 +0900

    add question test logic

--- src/pages/question.js
+++ src/pages/question.js
@@ -4,26 +4,73 @@ import { ProgressBar, Button } from "react-bootstrap";
 import { QuestionData } from "../assets/data/questiondata";
 
 const Question = () => {
+  const [questionNo, setQuestionNo] = React.useState(0);
+  const [totalScore, setTotalScore] = React.useState([
+    { id: "EI", score: 0 },
+    { id: "SN", score: 0 },
+    { id: "TF", score: 0 },
+    { id: "JP", score: 0 },
+  ]);
+
+  console.log("totalScore", totalScore);
+
+  const handleClickButtonA = (no, type) => {
+    if (type === "EI") {
+      // 기존 스코어에 더 할 값을 계산 (기존의 값 + 배점)
+      const addScore = totalScore[0].score + no;
+      // 새로운 객체 생성
+      const newObject = { id: "EI", score: addScore };
+      // splice(javascript 함수) 통해 새로운 객체를 해당 객체 자리에 넣어줌
+      totalScore.splice(0, 1, newObject);
+    } else if (type === "SN") {
+      const addScore = totalScore[1].score + no;
+
+      const newObject = { id: "SN", score: addScore };
+
+      totalScore.splice(1, 1, newObject);
+    } else if (type === "TF") {
+      const addScore = totalScore[2].score + no;
+
+      const newObject = { id: "TF", score: addScore };
+
+      totalScore.splice(2, 1, newObject);
+    } else {
+      const addScore = totalScore[3].score + no;
+
+      const newObject = { id: "JP", score: addScore };
+
+      totalScore.splice(3, 1, newObject);
+    }
+
+    setQuestionNo(questionNo + 1);
+  };
+
+  const handleClickButtonB = (no) => {
+    setQuestionNo(questionNo + 1);
+  };
+
   return (
     <Wrapper>
       <ProgressBar
         striped
         variant="danger"
-        now={80}
+        now={(questionNo / QuestionData.length) * 100}
         style={{ marginTop: "20px" }}
       />
-      <Title>{QuestionData[0].title}</Title>
+      <Title>{QuestionData[questionNo].title}</Title>
       <ButtonGroup>
         <Button
+          onClick={() => handleClickButtonA(1, QuestionData[questionNo].type)}
           style={{
             width: "40%",
             minHeight: "200px",
             fontSize: "15pt",
           }}
         >
-          {QuestionData[0].answera}
+          {QuestionData[questionNo].answera}
         </Button>
         <Button
+          onClick={() => handleClickButtonB(0, QuestionData[questionNo].type)}
           style={{
             width: "40%",
             minHeight: "200px",
@@ -31,7 +78,7 @@ const Question = () => {
             marginLeft: "20px",
           }}
         >
-          {QuestionData[0].answerb}
+          {QuestionData[questionNo].answerb}
         </Button>
       </ButtonGroup>
     </Wrapper>
