dd7085c57aeae95bfafeb099dfd2216af4df104a
jeongsangtae <wjdtkdxo55@naver.com>
2022-12-11 23:06:55 +0900

    result basic

--- src/pages/result.js
+++ src/pages/result.js
@@ -1,7 +1,65 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
+// css-in-js
+import styled from "styled-components";
+import Button from "react-bootstrap/Button";
+import CatImage from "../assets/logocat.jpg";
 
 const Result = () => {
-  return <div>결과화면입니다.</div>;
+  return (
+    <Wrapper>
+      <Header>예비집사 판별기</Header>
+      <Contents>
+        <Title>나에게 맞는 주인님은?</Title>
+        <LogoImage>
+          <img
+            src={CatImage}
+            className="rounded-circle"
+            width={350}
+            height={350}
+          />
+        </LogoImage>
+        <Desc>MBTI를 기반으로 하는 나랑 잘맞는 고양이 찾기!</Desc>
+        <Button style={{ fontFamily: "휴먼범석체" }}>테스트 시작하기</Button>
+      </Contents>
+    </Wrapper>
+  );
 };
 
 export default Result;
+
+const Wrapper = styled.div`
+  height: 100vh;
+  width: 100%;
+`;
+
+const Header = styled.div`
+  font-size: 40pt;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  font-family: "휴먼범석체";
+`;
+
+const Contents = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+`;
+
+const Title = styled.div`
+  font-size: 30px;
+  margin-top: 40px;
+  font-family: "휴먼범석체";
+`;
+
+const LogoImage = styled.div`
+  margin-top: 10px;
+`;
+
+const Desc = styled.div`
+  font-size: 20pt;
+  margin-top: 30px;
+  font-family: "휴먼범석체";
+`;
